2120ad59c04e855cb659c03b48b0e44b3b1506ba
karnell schultz <karnell.schultz@izettle.com>
2021-08-26 19:04:55 +0200

    clean files and add basic data to page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { usePerilContext } from "./PerilContext";
 
 function App() {
+  const perilData = usePerilContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<p>
+  Yyyyyy
+</p>
+
+<h2>We have you covered
+</h2>
+      {perilData ? (
+        perilData.map((el) => (
+          <div key={el.title}>
+            <h2>{el.title}</h2>
+            <img
+              src={el.icon.variants.light.svgUrl}
+              height="24px"
+              alt={el.title}
+            ></img>
+            <p>{el.description}</p>
+            <ul>
+              {el.covered.map((covered) => (
+                <li> ✅ {covered}</li>
+              ))}
+            </ul>
+            <ul>
+              {el.exceptions.map((exceptions) => (
+                <li> ❌ {exceptions}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <div>...loading </div>
+      )}
+    </>
   );
 }
 
