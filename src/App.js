e46c9bb210c39ee31c07f8a818f1b2991236c924
karnell schultz <karnell.schultz@izettle.com>
2021-08-27 17:02:45 +0200

    added styles for cards and layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import React from "react";
-import { usePerilContext } from "./PerilContext";
+import { usePerilContext, Status } from "./PerilContext";
 
 function App() {
-  const perilData = usePerilContext();
+  const { status, data } = usePerilContext();
+  console.log(status);
   return (
     <>
-<p>
-  Yyyyyy
-</p>
+      <h2>We have you covered</h2>
 
-<h2>We have you covered
-</h2>
-      {perilData ? (
-        perilData.map((peril) => (
-          <div key={peril.title}>
-            <h2>{peril.title}</h2>
-            <img
-              src={peril.icon.variants.light.svgUrl}
-              height="24px"
-              alt={peril.title}
-            ></img>
-            <p>{peril.description}</p>
-            <ul>
-              {peril.covered.map((covered) => (
-                <li> ✅ {covered}</li>
-              ))}
-            </ul>
-            <ul>
-              {peril.exceptions.map((exceptions) => (
-                <li> ❌ {exceptions}</li>
-              ))}
-            </ul>
-          </div>
-        ))
-      ) : (
-        <div>...loading </div>
-      )}
+      <p>
+        Extensive coverage for you and your family, your house and your
+        belongings. All risk is always included. Click the icons for more info.
+      </p>
+
+      <h3>Home Insurance Renter</h3>
+
+      {status === Status.loading && <h1>...loading </h1>}
+
+      <div className="container">
+        {status === Status.done &&
+          data.map((peril) => (
+            <div className="card" key={peril.title}>
+              <img
+                src={peril.icon.variants.light.svgUrl}
+                height="48px"
+                width="48px"
+                alt={peril.title}
+              ></img>
+              <p>{peril.title}</p>
+
+              {/* <p>{peril.description}</p> */}
+              {/* <ul>
+                {peril.covered.map((covered) => (
+                  <li> ✅ {covered}</li>
+                ))}
+              </ul> */}
+              {/* <ul>
+                {peril.exceptions.map((exceptions) => (
+                  <li> ❌ {exceptions}</li>
+                ))}
+              </ul> */}
+            </div>
+          ))}
+      </div>
     </>
   );
 }
